73c7017aff54b9675a119293c392324b121243f0
github username <github email address>
2019-11-08 10:51:51 -0500

    Done.

---
+++ src/message.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Messages = () => {
+    return(
+      <label> Message: 
+        <input type="text">
+  
+        </input>
+      </label>
+    )
+};
+
+export default Messages
